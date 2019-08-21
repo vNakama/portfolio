13c10a9a142df87635886c85eb12c8b06efc261a
vNakama <nakama@alunos.utfpr.edu.br>
2019-08-21 18:54:08 -0300

    Updates

---
+++ precache-manifest.36708695e781ee8d4200f2005f9ba831.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c126b1c1465ea3d0d639f44fe6b47d5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7b3486aa926975dcb0ff",
+    "url": "/portfolio/static/css/main.cc8cc5e2.chunk.css"
+  },
+  {
+    "revision": "e46f63530a6cf54bc1b9",
+    "url": "/portfolio/static/js/2.70afb614.chunk.js"
+  },
+  {
+    "revision": "7b3486aa926975dcb0ff",
+    "url": "/portfolio/static/js/main.08abf2fc.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "69cd4ea5a136a56cafe7ccddbfc156ed",
+    "url": "/portfolio/static/media/1.69cd4ea5.png"
+  },
+  {
+    "revision": "c1e06cbc9f054d2ecd7bc2781ce04938",
+    "url": "/portfolio/static/media/10.c1e06cbc.png"
+  },
+  {
+    "revision": "abe476e556290955ad2f9f873cd5490f",
+    "url": "/portfolio/static/media/11.abe476e5.png"
+  },
+  {
+    "revision": "cd39e403152327f953ef90c1537e5cc6",
+    "url": "/portfolio/static/media/12.cd39e403.png"
+  },
+  {
+    "revision": "5177e1d7da5ff65f80dd7acdd37280f0",
+    "url": "/portfolio/static/media/13.5177e1d7.png"
+  },
+  {
+    "revision": "93fdaad04e450339fd5841838c8223c4",
+    "url": "/portfolio/static/media/14.93fdaad0.png"
+  },
+  {
+    "revision": "002f4d5e7a917a9ee39bbdeb1f312fbf",
+    "url": "/portfolio/static/media/15.002f4d5e.png"
+  },
+  {
+    "revision": "055dcb552c8314ea6e5a22b06e82b87f",
+    "url": "/portfolio/static/media/16.055dcb55.jpg"
+  },
+  {
+    "revision": "bb9a607fef060a197b9714f50ba716b5",
+    "url": "/portfolio/static/media/16.bb9a607f.png"
+  },
+  {
+    "revision": "11995556966a408018352113a1d6c7eb",
+    "url": "/portfolio/static/media/17.11995556.png"
+  },
+  {
+    "revision": "b4c9a3e91ec830110a1b9e6fac8811a3",
+    "url": "/portfolio/static/media/17.b4c9a3e9.jpeg"
+  },
+  {
+    "revision": "22ca971aaca05ca0cd67d1f9842e9763",
+    "url": "/portfolio/static/media/18.22ca971a.png"
+  },
+  {
+    "revision": "f36575923b4917aa55bd4dda0292cc53",
+    "url": "/portfolio/static/media/19.f3657592.png"
+  },
+  {
+    "revision": "625c7d133a024c48b7e7b82f120f8ed2",
+    "url": "/portfolio/static/media/2.625c7d13.png"
+  },
+  {
+    "revision": "98d77782a5b0e226bee8037984e58577",
+    "url": "/portfolio/static/media/21.98d77782.png"
+  },
+  {
+    "revision": "2e7c405bd20d42454f1ca3c2d0434e7f",
+    "url": "/portfolio/static/media/22.2e7c405b.png"
+  },
+  {
+    "revision": "e63ee6b786060812a9c3177a2583341a",
+    "url": "/portfolio/static/media/23.e63ee6b7.png"
+  },
+  {
+    "revision": "079ac17244cba0bf370500a65ddf3296",
+    "url": "/portfolio/static/media/3.079ac172.png"
+  },
+  {
+    "revision": "b350639fe93c08a01e5958d9addb990b",
+    "url": "/portfolio/static/media/4.b350639f.png"
+  },
+  {
+    "revision": "ef09f57674e1c405210b699775bdd4bd",
+    "url": "/portfolio/static/media/5.ef09f576.png"
+  },
+  {
+    "revision": "a0c5e3cfe38a26ceea8db2fa4a539d4b",
+    "url": "/portfolio/static/media/6.a0c5e3cf.png"
+  },
+  {
+    "revision": "4935b2c5278da50bbe48310f3131d83c",
+    "url": "/portfolio/static/media/7.4935b2c5.png"
+  },
+  {
+    "revision": "629c738255045448c4faebe1a0bb48fe",
+    "url": "/portfolio/static/media/8.629c7382.png"
+  },
+  {
+    "revision": "536896750ce724763f222fdf840d9118",
+    "url": "/portfolio/static/media/9.53689675.png"
+  },
+  {
+    "revision": "6ff030eab6f301ea65ae2db3765d98df",
+    "url": "/portfolio/static/media/curriculum-vitae.6ff030ea.pdf"
+  },
+  {
+    "revision": "fb0be3ff9f12967c0bfde7b2626e7ac9",
+    "url": "/portfolio/static/media/perfil.fb0be3ff.png"
+  }
+]);
